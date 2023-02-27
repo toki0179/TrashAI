fe367073ee8f31357b457063405c83ab356df13f
toki0179 <olliwes01@gmail.com>
2023-02-27 20:28:42 +0000

    Changed status

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new discord.Client({
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
-    client.user.setActivity("with my crack pipe");
+    client.user.setActivity(`This is a shitty ai`);
 })
 
 client.on('messageCreate', async (message) => {
